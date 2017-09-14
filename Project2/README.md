7224e0a0f07912b7df0f3e1ddc7970f38b86cb65
Erlend Lima <ErlendLima@outlook.com>
2017-09-14 14:07:55 +0200

    Init Project 2

---
+++ Project2/README.md
@@ -0,0 +1,48 @@
+# Project One
+
+<p align="center">
+<b><a href="#usage">Usage</a></b>
+|
+<b><a href="#testing">Testing</a></b>
+|
+<b><a href="#credits">Credits</a></b>
+|
+<b><a href="#license">License</a></b>
+</p>
+<br>
+
+## Usage
+The project is divided into source code and the written rapport. The source code
+is located across two different directories: analysis and cpp. The 
+resulting data produced by the C++ program is read by the analysis code located
+in analysis. 
+
+Compilation of the C++ program requires C++14, Armadillo version 6.7 and CMake version 3.1. If the 
+requirements are satisfied, the makefile can be created and run with
+
+```console
+cmake . && make
+```
+
+After initial compilation, typing `make` will make the program. To run the
+compiled program, type `./solve`. Optional flags exist, see `./solve -h`.
+The resulting data can then be analyzed 
+by the python script in the analysis directory using `python analyze.py ../cpp/data`.
+
+The rapport and all supporting material is located in the `latex` directory. 
+The document is compatible with PdfLaTeX, LuaTeX and XeLaTeX.
+
+## Testing
+The framework [Google Test](https://github.com/google/googletest) is used to write unit tests.
+This is fully integrated into CMake, making it easy to both add and run the tests. In order
+to compile the tests, type `cmake -Dtest=ON`. For actually running the tests, run `./runUnitTests` or
+`make test`.
+
+Better support for debugging and stricter compiler flags can be turned on using `cmake -DCMAKE_BUILD_TYPE=DEBUG`.
+
+## Credits
+The contributors of this project are Erlend Lima, Frederik Johan Mellbye, Aram Salihi and Halvard Sutterud.
+
+## License
+This project is licensed under the terms of the **MIT** license.
+You can check out the full license [here](../LICENSE)
---
+++ Project2/latex/auto/rapport.el
@@ -0,0 +1,84 @@
+(TeX-add-style-hook
+ "rapport"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("revtex4-1" "aps" "reprint")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("babel" "english") ("xcolor" "usenames" "dvipsnames" "svgnames" "table") ("caption" "font={scriptsize}") ("listings" "procnames") ("algpseudocode" "noend")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "revtex4-1"
+    "revtex4-110"
+    "fontenc"
+    "inputenc"
+    "fontspec"
+    "babel"
+    "csquotes"
+    "url"
+    "textcomp"
+    "xcolor"
+    "caption"
+    "amsmath"
+    "amsthm"
+    "amsfonts"
+    "amssymb"
+    "enumerate"
+    "tikz"
+    "float"
+    "listings"
+    "pstool"
+    "pgfplots"
+    "wrapfig"
+    "graphicx"
+    "epstopdf"
+    "afterpage"
+    "physics"
+    "multirow"
+    "gensymb"
+    "algorithm"
+    "microtype"
+    "algpseudocode"
+    "colortbl"
+    "geometry"
+    "hyperref"
+    "subcaption"
+    "lipsum"
+    "nth"
+    "siunitx")
+   (TeX-add-symbols
+    '("nonref" 1)
+    '("noref" 1)
+    '("unit" 1)
+    "blankpage"
+    "numberthis")
+   (LaTeX-add-labels
+    "sec:introduction"
+    "sec:theory"
+    "sec:method"
+    "sec:results"
+    "sec:discussion"
+    "sec:conclusion")
+   (LaTeX-add-bibliographies
+    "references")
+   (LaTeX-add-xcolor-definecolors
+    "dkgreen"
+    "dred"
+    "dblue"
+    "lgrey"
+    "gray"
+    "darkblue")
+   (LaTeX-add-caption-DeclareCaptions
+    '("\\DeclareCaptionFont{white}" "Font" "white")
+    '("\\DeclareCaptionFormat{listing}" "Format" "listing")))
+ :latex)
+
