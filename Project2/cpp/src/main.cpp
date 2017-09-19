4edf202e449252b30defc424f40dc0235f7b10a4
Fred Mell <frederikmellbye@me.com>
2017-09-19 21:30:08 +0200

    mess before pull

---
+++ Project2/cpp/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <memory.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <armadillo>
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
