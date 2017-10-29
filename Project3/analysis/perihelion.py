6e5f36ed0f7f7d163d54dc94f294b86e3eec1a01
Erlend Lima <ErlendLima@outlook.com>
2017-10-29 20:05:41 +0100

    Finish report

--- Project3/analysis/perihelion.py
+++ Project3/analysis/perihelion.py
@@ -9,14 +9,14 @@ class Perihelion(Runner):
     def setup(self):
         self['number of years'] = 100
         self['do save results'] = False
-        self['do save any results'] = True
+        self['do save any results'] = False
         self['use all planets'] = False
         self['save period'] = 10000
         self['use planets'] = ['Sun', 'Mercury']
-        self['steps per year'] = 1e6
+        self['steps per year'] = 1e7
         self['freeze sun'] = True
         self['use two body approximation'] = False
-        self['use relativistic correction'] = True
+        self['use relativistic correction'] = False
         self['method'] = 'verlet'
         self.get_planet('Sun')['position'] = [0.0, 0.0, 0.0]
         self.get_planet('Sun')['Velocity'] = [0.0, 0.0, 0.0]
@@ -26,17 +26,19 @@ class Perihelion(Runner):
     def run(self):
         out, _ = self.run_simulation()
         print(out)
-        self.run_analysis()
         self.getPerihelion()
 
     def getPerihelion(self):
         timenangle = np.loadtxt("../data/precession.txt");
-        # print(timenangle.shape)
-        # print(timenangle[:, 1] - timenangle[0, 1])
-        plt.plot(timenangle[:, 0], (timenangle[:, 1]-timenangle[0,1]))
-        plt.xlabel("Time [yr]")
-        plt.ylabel("Precession angle")
-        plt.show()
+        fig = plt.figure(figsize=(11, 8))
+        ax = fig.add_subplot(111)
+        relative = timenangle[:, 1]-timenangle[0, 1]
+        ax.plot(timenangle[:, 0], relative)
+        ax.set_xlabel("Time [yr]")
+        ax.set_ylabel("Precession angle relative to 0")
+        print("Endpoint:", relative[-1], relative[-1]*180/np.pi * 3600)
+        fig.savefig("../latex/figures/newtonprecession1.eps", dpi=1200)
+        # plt.show()
 
 
 if __name__ == '__main__':
