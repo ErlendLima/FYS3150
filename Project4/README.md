c020f6a6e4132c916563dc024ce5e5c43dc796fa
Erlend Lima <ErlendLima@outlook.com>
2017-10-31 10:58:12 +0100

    Add Project

---
+++ Project4/README.md
@@ -0,0 +1,108 @@
+# Project Four
+
+![Travis CI](https://travis-ci.org/Caronthir/FYS3150.svg?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/Caronthir/FYS3150/badge.svg?branch=master)](https://coveralls.io/github/Caronthir/FYS3150?branch=master)
+<a href="https://scan.coverity.com/projects/fys3150">
+  <img alt="Coverity Scan Build Status"
+       src="https://scan.coverity.com/projects/14080/badge.svg"/>
+</a>
+
+<p align="center">
+<b><a href="#overview">Overview</a></b>
+|
+<b><a href="#analysis-scripts">Analysis Scripts</a></b>
+|
+<b><a href="#usage-c">Usage (C++)</a></b>
+|
+<b><a href="#testing-c">Testing (C++)</a></b>
+|
+<b><a href="#usage-julia">Usage (Julia)</a></b>
+|
+<b><a href="#credits">Credits</a></b>
+|
+<b><a href="#license">License</a></b>
+</p>
+<br>
+
+
+<!-- <img height="700px" align="center" src="latex/figures/solarsys.png?raw=true"> -->
+![Animation of the solar system](https://github.com/Caronthir/FYS3150/blob/master/Project3/latex/figures/animation.gif)
+
+## Overview
+The aim of [Project 4](https://compphysics.github.io/ComputationalPhysics/doc/Projects/2017/Project4/pdf/Project4.pdf) in FYS3150
+is to develop code for simulating the solar system using Euler's Forward method and Velocity Verlet.
+This is implemented in both C++ and Julia, and further analysis and plotting is done by Python. 
+Python is also used for collecting the initial conditions from [NASA](https://ssd.jpl.nasa.gov/?horizons).
+The report itself and associated LaTeX files are available in the `latex` directory.
+
+## Analysis Scripts
+
+There are several Python scripts which do the analysis of the output from the C++ program. Here is a
+table explaining the function to each script. Python 3.6 is required.
+
+| Script                  | Function                                                                                                          |
+| ------                  | --------                                                                                                          |
+| `getnasa.py`            | Contacts NASA through telnet and downloads all bodies in the targets.txt file into a JSON file.                   |
+| `analyze.py`            | A general script which plots the position, energy and angular moment from the output. Can also animate the orbits.|
+| `batchrun.py`           | Compares Euler and Verlet in terms of running time.                                                               |
+| `findescapevelocity.py` | Uses a bijection method to find the escape velocity of Earth.                                                     |
+| `jupiterrunner.py`      | Solves the 3-body problem consisting of the Sun, Earth and Jupiter.                                               |
+| `perihelion.py`         | Measure the perihelion precession of Mercury from relativistic effects.                                 |
+
+
+## Usage (C++)
+
+Compilation of the C++ program requires C++14, Armadillo version 6.7, CMake version 3.1 and JSONcpp. On Ubuntu these can be installed with
+```console
+sudo apt-get install cmake liblapack-dev libblas-dev libbost-dev libarmadillo-dev libjsoncpp-1 libjsoncpp-dev
+```
+Note that the path to jsoncpp is not standardized. This program assumes it lies in `<include path>/jsoncpp/json/json.h`. 
+If you get a compilation error, create a symlink from the path to your `json.h` to `jsoncpp/json/json.h`.
+
+If the
+requirements are satisfied, the makefile can be created and run with
+
+```console
+cmake . && make
+```
+
+After initial compilation, typing `make` will make the program. To run the
+compiled program, type `./solve`. Optional flags exist, see `./solve -h`.
+The resulting data can then be analyzed
+by the python script in the analysis directory using `python analyze.py ../cpp/data`.
+
+
+The rapport and all supporting material is located in the `latex` directory.
+The document is compatible with PdfLaTeX, LuaTeX and XeLaTeX.
+
+## Testing (C++)
+Testing is not implemented in the C++ version.
+<!-- The framework [Google Test](https://github.com/google/googletest) is used to write unit tests. Google test is -->
+<!-- imported as git submodule. To make it work, type -->
+
+<!-- ```console -->
+<!-- git submodule init -->
+<!-- git submodule update -->
+<!-- ``` -->
+
+<!-- Google test is fully integrated into CMake, making it easy to both add and run the tests. In order -->
+<!-- to compile the tests, type `cmake -Dtest=ON`. For actually running the tests, run `./runUnitTests` or -->
+<!-- `make test`. -->
+
+<!-- Better support for debugging and stricter compiler flags can be turned on using `cmake -DCMAKE_BUILD_TYPE=DEBUG`. -->
+
+## Usage (Julia)
+The Julia version is not completed, and does not count as a part of this assignment. 
+This is left here to stand as a testament to the hubris of students.
+
+The version of Julia required is at least 0.6.0
+
+These can be tested by running
+`julia test.jl`.
+
+## Credits
+The contributors of this project are Erlend Lima and Frederik Johan Mellbye.
+
+## License
+This project is licensed under the terms of the **MIT** license.
+You can check out the full license [here](../LICENSE)
---
+++ Project4/analysis/analyze.py
@@ -0,0 +1,173 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import argparse
+import os
+
+sns.set(context="poster")
+
+
+class Analyzer:
+    def __init__(self, path, position, energy, angular_momentum, plot2d=False):
+        self.position = self.load(path, position)
+        N, M = self.position.shape
+        self.position = self.position.reshape(N, 3, M//3, order='F')
+
+        self.energy = self.load(path, energy)
+        self.angular_momentum = self.load(path, angular_momentum)
+        self.plot2d = plot2d
+
+    def load(self, path, name):
+        path = os.path.join(path, name)
+        data = np.loadtxt(path)
+        return data
+
+    def plot(self):
+        self.plot_position()
+        self.plot_angular_momentum()
+        self.plot_energy()
+
+    def plot_position(self):
+        fig = plt.figure(figsize=(9, 7))
+        if not self.plot2d:
+            ax = fig.add_subplot(111, projection='3d')
+            # ax.scatter(*self.position[0, :, 0], marker='o',
+                       # c=(1, 0.6, 0), s=200)
+            for n in range(self.position.shape[2]):
+                ax.plot(*self.position[:, :, n].T)
+            xmin = np.min(self.position[:, 0, :])
+            ymin = np.min(self.position[:, 1, :])
+            zmin = np.min(self.position[:, 2, :])
+            xmax = np.max(self.position[:, 0, :])
+            ymax = np.max(self.position[:, 1, :])
+            zmax = np.max(self.position[:, 2, :])
+            axmax = max([xmax, ymax, zmax])
+            axmin = min([xmin, ymin, zmin])
+            for axlim in [ax.set_xlim3d, ax.set_ylim3d, ax.set_zlim3d]:
+                axlim([axmin, axmax])
+        else:
+            ax = fig.add_subplot(111)
+            # ax.scatter(*self.position[0, 0:2, 0], marker='o',
+                       # c=(1, 0.6, 0), s=200)
+            for n in range(self.position.shape[2]):
+                ax.plot(*self.position[:, 0:2, n].T)
+            for n in range(self.position.shape[2]):
+                ax.plot(*self.position[-1, 0:2, n].T, 'o')
+        # ax.legend()
+        ax.set_xlabel('X [AU]')
+        ax.set_ylabel('Y [AU]')
+        if not self.plot2d:
+            ax.set_zlabel('Z [AU]')
+        else:
+            ax.axis('equal')
+
+        plt.show()
+        fig.savefig('../latex/figures/position.eps', dpi=1200)
+
+    def plot_angular_momentum(self):
+        fig = plt.figure(figsize=(9, 7))
+        ax = fig.add_subplot(111)
+        time = self.energy[0, :]
+        ax.plot(time, self.angular_momentum)
+        ax.set_xlabel(r"Time [yr]")
+        ax.set_ylabel(r"Total Angular Momentum [AU$^2$/yr]")
+        fig.savefig('../latex/figures/angularmom.eps', dpi=1200)
+        plt.show()
+
+    def plot_energy(self):
+        fig_energy, (kinetic, potential, total) = plt.subplots(3, sharex=True,
+                                                               figsize=(11, 8))
+        self.energy = self.energy[:, 1:-2]
+        # Time
+        time = self.energy[0, :]
+        # Kinetic Energy
+        Ek = self.energy[1, :]/self.energy[1, 0]
+        kinetic.plot(time, Ek)
+        # Potential Energy
+        Ep = self.energy[2, :]/self.energy[2, 0]
+        potential.plot(time, Ep)
+        # Total Energy
+        Et = np.sum(self.energy[[1, 2], :], axis=0)/np.sum(self.energy[[1, 2], 0], axis=0)
+        total.plot(time, Et)
+        total.set_xlabel("Time [years]")
+        kinetic.set_title("Kinetic Energy")
+        potential.set_title("Potential Energy")
+        total.set_title("Total Energy")
+        plt.show()
+        fig_energy.savefig('../latex/figures/energy.eps', dpi=1200)
+
+    def update_lines(self, num, lines, scatter):
+        start = 0 if num < 1000 else num-1000
+        for index, (line, scatt) in enumerate(zip(lines, scatter)):
+            line[0].set_data(*self.position[start:num+1, 0:2, index].T)
+            line[0].set_3d_properties(self.position[start:num+1, 2, index])
+            scatt[0].set_data(*self.position[num:num+1, 0:2, index].T)
+            scatt[0].set_3d_properties(self.position[num:num+1, 2, index])
+        return lines
+
+    def animate(self, save=False):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # Set the limits of the axis to the greatest of all x-, y- and z-axes.
+        xmin = np.min(self.position[:, 0, :])
+        ymin = np.min(self.position[:, 1, :])
+        zmin = np.min(self.position[:, 2, :])
+        xmax = np.max(self.position[:, 0, :])
+        ymax = np.max(self.position[:, 1, :])
+        zmax = np.max(self.position[:, 2, :])
+        axmax = max([xmax, ymax, zmax])
+        axmin = min([xmin, ymin, zmin])
+        for axlim in [ax.set_xlim3d, ax.set_ylim3d, ax.set_zlim3d]:
+            axlim([axmin, axmax])
+
+        ax.set_xlabel('X [AU]')
+        ax.set_ylabel('Y [AU]')
+        ax.set_zlabel('Z [AU]')
+        ax.grid(False)
+
+        lines = [ax.plot(*self.position[:1, :, n].T, alpha=0.5)
+                 for n in range(self.position.shape[2])]
+        scatter = [ax.plot(*self.position[:1, :, n].T, linestyle='', marker='o')
+                   for n in range(self.position.shape[2])]
+
+        system_animation = animation.FuncAnimation(fig, self.update_lines,
+                                                   frames=range(0, self.position.shape[0], 500),
+                                                   fargs=(lines, scatter,),
+                                                   interval=10, blit=False)
+        if save:
+            system_animation.save('../latex/figures/animation.gif',
+                                  writer='imagemagick', fps=30)
+        else:
+            plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Analyzes data for project 3')
+    parser.add_argument('--search_path', type=os.path.abspath,
+                        default='../data/',
+                        help="Directory to search for input files")
+    parser.add_argument('--position', type=str, default="position.txt",
+                        help="The file containing the positions")
+    parser.add_argument('--energy', type=str, default="energy.txt",
+                        help="The file containing the energies")
+    parser.add_argument('--angular', type=str, default="angmom.txt",
+                        help="The file containing the angular momenta")
+    parser.add_argument('--plot2d', help="Plot as 2D",
+                        action="store_true")
+    parser.add_argument('--animate', help="Animate the orbits",
+                        action="store_true")
+    parser.add_argument('--save', help="Save the (animation) plots",
+                        action="store_true")
+    args = parser.parse_args()
+    analyzer = Analyzer(args.search_path, args.position,
+                        args.energy, args.angular,
+                        plot2d=args.plot2d)
+    if args.animate:
+        analyzer.animate(args.save)
+    else:
+        analyzer.plot()
