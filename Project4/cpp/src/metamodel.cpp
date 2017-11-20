8bbf2337f5c822b540e944c9c5fd831c1ef04d1e
fredmell <frederikmellbye@me.com>
2017-11-20 16:13:50 +0100

    Mess

--- Project4/cpp/src/metamodel.cpp
+++ Project4/cpp/src/metamodel.cpp
@@ -108,7 +108,7 @@ void Metamodel::saveExpectationValues(std::ofstream& stream, std::vector<double>
   double expectM        = expVals[2]*factor;
   double expectMSquared = expVals[3]*factor;
   double expectMFabs    = expVals[4]*factor;
-  double n_flips        = expVals[5];
+  double n_flips        = expVals[5]*spinNorm/numProcessors;
 
   double varE = (expectESquared - expectE*expectE)*spinNorm;
   double varM = (expectMSquared - expectMFabs*expectMFabs)*spinNorm;
